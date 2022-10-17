8994aa30dea613106b51de4dc23e2e4f01cca324
bluesyu <bluesedenyu@gmail.com>
2022-10-18 03:00:43 +0800

    debug: trying to build static site

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: 'https://twitter.com/bluesedenyu',
   seo: {
-    keywords: ['Blog', 'Website', 'Notion', 'Tech', 'Python', 'Django'],
+    keywords: ['Blog', 'Notion', 'Tech', 'Python', 'Django'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   // notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
