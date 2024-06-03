e859b1c56e17fa48ade5c565ea45d3d3d9775e26
Musher <593905676@qq.com>
2024-06-03 10:22:11 +0800

    新增ga配置

--- blog.config.js
+++ blog.config.js
@@ -34,7 +34,7 @@ const BLOG = {
       domainId: "", // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: "", // e.g: G-XXXXXXXXXX
+      measurementId: "G-7BVRLGN9PG", // e.g: G-XXXXXXXXXX
     },
   },
   comment: {
