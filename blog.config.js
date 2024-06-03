7dd61d6d0ba7943ba5e1dddeed23b04d988f5a29
Musher <593905676@qq.com>
2024-06-03 12:42:32 +0800

    修复评论系统（应该是已经修复了）

--- blog.config.js
+++ blog.config.js
@@ -49,7 +49,7 @@ const BLOG = {
       distractionFreeMode: false,
     },
     utterancesConfig: {
-      repo: "MusherM/musherm.github.io",
+      repo: "MusherM/nobelium",
     },
     cusdisConfig: {
       appId: "", // data-app-id
